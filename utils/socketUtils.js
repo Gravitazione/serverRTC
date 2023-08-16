b8c91bad865237b7aec1046ac08bfcd882c31523
Sasipasch <sasipas.ch@chococrm.com>
2023-08-16 09:32:39 +0700

    feat: initial proj test

---
+++ utils/socketUtils.js
@@ -0,0 +1,44 @@
+const socketIO = require("socket.io"); // Import socket.io
+
+const socketNames = {}; // Define socketNames here
+
+exports.sio = (server) => {
+  return socketIO(server, {
+    transport: ["polling"],
+    cors: {
+      origin: "*",
+    },
+  });
+};
+
+exports.connection = (io) => {
+  io.on("connection", (socket) => {
+    console.log(`${socket.id} is connected`);
+
+    socket.on("message", (message) => {
+      console.log(`message from ${socket.id}: ${message}`);
+      io.to(socket.room).emit("control", {
+        name: socketNames[socket.id],
+        text: message,
+        room: socket.room,
+      });
+    });
+
+    socket.on("join_room", (room) => {
+      if (socket.room) {
+        socket.leave(socket.room); // Leave the current room before joining a new one
+      }
+      socket.room = room; // Store the room name in the socket
+      socket.join(room); // Join the specified room
+    });
+
+    socket.on("disconnect", () => {
+      console.log(`socket ${socket.id} disconnected`);
+      delete socketNames[socket.id]; // Remove the name when disconnected
+    });
+
+    socket.on("set_name", (name) => {
+      socketNames[socket.id] = name;
+    });
+  });
+};
